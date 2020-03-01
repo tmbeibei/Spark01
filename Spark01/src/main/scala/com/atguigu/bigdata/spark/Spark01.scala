e82eee6c77ad5a6d2b7d3ba26f07cae20ace312e
tianmin <tmbeibei@aliyun.com>
2020-03-01 08:14:21 +0800

    spark01 第一次提交

---
+++ Spark01/src/main/scala/com/atguigu/bigdata/spark/Spark01.scala
@@ -0,0 +1,12 @@
+package com.atguigu.bigdata.spark
+
+/**
+ * @author tianmin
+ * @date 2020/3/1 0001
+ * @notes
+ */
+class Spark01 {
+  def sayHello(): Unit ={
+    println("hhell")
+  }
+}
---
+++ SparkStrem/src/main/scala/com/atguigu/bigdata/spark/SparkTest01.scala
@@ -0,0 +1,12 @@
+package com.atguigu.bigdata.spark
+
+/**
+ * @author tianmin
+ * @date 2020/3/1 0001
+ * @notes
+ */
+object SparkTest01 {
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+  }
+}
